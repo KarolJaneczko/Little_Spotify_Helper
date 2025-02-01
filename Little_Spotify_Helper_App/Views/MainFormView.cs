42892b6a88b7184a36d876a1e25f9dfd7fef466b
Karol Janeczko <97122972+KarolJaneczko@users.noreply.github.com>
2025-02-01 01:31:17 +0100

    wrzutka

--- Little_Spotify_Helper_App/Views/MainFormView.cs
+++ Little_Spotify_Helper_App/Views/MainFormView.cs
@@ -1,7 +1,14 @@
+using NAudio.Wave;
+
 namespace Little_Spotify_Helper_App {
     public partial class MainFormView : Form {
         public MainFormView() {
             InitializeComponent();
+
+            var reader = new Mp3FileReader("Blade_-_Vampire_Dance_Club_Theme.mp3");
+            var waveOut = new WaveOut(); // or WaveOutEvent()
+            waveOut.Init(reader);
+            waveOut.Play();
         }
     }
 }
